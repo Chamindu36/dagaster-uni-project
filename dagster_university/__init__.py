97763baceecd6a283819b6772a9f463e585c2a70
Chamindu <cudakara@gmail.com>
2024-08-23 15:44:50 +0200

    add assets to daster pipeline

--- dagster_university/__init__.py
+++ dagster_university/__init__.py
@@ -1,11 +1,12 @@
 # fmt: off
 from dagster import Definitions, load_assets_from_modules
 
-from .assets import metrics, trips
+from .assets import metrics, trips, zones
 
 trip_assets = load_assets_from_modules([trips])
+zone_aaets = load_assets_from_modules([zones])
 metric_assets = load_assets_from_modules([metrics])
 
 defs = Definitions(
-    assets=[*trip_assets, *metric_assets]
+    assets=[*trip_assets, *metric_assets, *zone_aaets],
 )
---
+++ dagster_university/assets/zones.py
@@ -0,0 +1,34 @@
+import requests
+from . import constants
+from dagster import asset
+import duckdb
+import os
+@asset
+def taxi_zones_file() -> None:
+    """
+      The raw CSV file for the taxi zones dataset. Sourced from the NYC Open Data portal.
+    """
+    raw_taxi_zones = requests.get(
+        "https://data.cityofnewyork.us/api/views/755u-8jsi/rows.csv?accessType=DOWNLOAD"
+    )
+
+    with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
+        output_file.write(raw_taxi_zones.content)
+
+@asset(
+    deps=["taxi_zones_file"]
+)
+def taxi_zones() -> None:
+    sql_query = f"""
+        create or replace table zones as (
+            select
+                LocationID as zone_id,
+                zone,
+                borough,
+                the_geom as geometry
+            from '{constants.TAXI_ZONES_FILE_PATH}'
+        );
+    """
+
+    conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
+    conn.execute(sql_query)
