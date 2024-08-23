65f1f5b94fca749b51d5ee14fdae66343fdf61e6
Chamindu <cudakara@gmail.com>
2024-08-23 15:54:14 +0200

    add dependancies and representations to daster pipeline

--- dagster_university/__init__.py
+++ dagster_university/__init__.py
@@ -1,11 +1,12 @@
 # fmt: off
 from dagster import Definitions, load_assets_from_modules
 
-from .assets import metrics, trips, zones
+from .assets import metrics, trips, zones, trips_by_week
 
 trip_assets = load_assets_from_modules([trips])
 zone_aaets = load_assets_from_modules([zones])
 metric_assets = load_assets_from_modules([metrics])
+metric_assets += load_assets_from_modules([trips_by_week])
 
 defs = Definitions(
     assets=[*trip_assets, *metric_assets, *zone_aaets],
